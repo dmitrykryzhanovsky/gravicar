ff76bc905a583953f3e9b95dd08a9920e17b4ebf
dmitrykryzhanovsky <dmitry.kryzhanovsky@gmail.com>
2020-01-08 20:54:22 +0400

    Решил изменить архитектуру. Удалил ненужные файлы

--- ARDrone3/Communication/EFrameTargetBufferId.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ARDrone3.Communication
-{
-    /// <summary>
-    /// Дополнительная характеристика данных, содержащихся в фрейме.
-    /// </summary>
-    public enum EFrameTargetBufferId : byte
-    {
-    }
-}
