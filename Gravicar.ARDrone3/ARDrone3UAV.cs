20224e0afc33dbe30e677c55b443d27e2ae09ffe
dmitrykryzhanovsky <dmitry.kryzhanovsky@gmail.com>
2020-05-16 12:10:27 +0400

    Завершил перенос кода

--- Gravicar.ARDrone3/ARDrone3UAV.cs
+++ Gravicar.ARDrone3/ARDrone3UAV.cs
@@ -1,4 +1,10 @@
-﻿using Gravicar.ARDrone3.Commands;
+﻿using System;
+using System.Net.Sockets;
+
+// Глянуть, нет ли в .NET Standard базового класса для UdpClient и TcpClient
+//using Newtonsoft.Json.Linq;
+
+using Gravicar.ARDrone3.Commands;
 
 namespace Gravicar.ARDrone3
 {
@@ -11,13 +17,67 @@ namespace Gravicar.ARDrone3
         /// <summary>
         /// Выполняет первоначальный поиск дрона при установке сетевого соединения.
         /// </summary>
+        /// <param name="responseJson">JSON,возвращаемый дроном. Если при установке соединения или записи / чтении данных произошла 
+        /// ошибка, <paramref name="responseJson"/> будет равен String.Empty. Подробнее о формате JSON’ов, пересылаемых при поиске 
+        /// дрона и установке первичного соединения, см. ARSDK_Protocols.pdf, раздел 1.2.1.</param>
         /// <returns><list type="bullet">
         /// <item>TRUE – операция прошла успешно, соединение установлено корректно.</item>
-        /// <item>FALSE – что-то пошло не так.</item>
+        /// <item>FALSE – что-то пошло не так.Возможные причины:<list type="bullet">
+        ///         <item>не установлено соединение;</item>
+        ///         <item>произошла ошибка во время передачи или приёма данных;</item>
+        ///         <item>поле status в JSON-запросе, пришедшем от дрона, не равно Const.Discovering.VToC_ConnectionIsCorrect"/>.</item>
+        ///     </list></item>
         /// </list></returns>
-        public bool Discover ()
+        public bool Discover (out string responseJson)
         {
-            throw new System.NotImplementedException ();
+            bool          successedOperation;
+            TcpClient     connection = new TcpClient ();
+            NetworkStream stream     = null;
+
+            try
+            {
+                // Устанавливаем соединение для поиска дрона.
+                connection.Connect (Const.NetworkConnection.DroneHostAddress, Const.NetworkConnection.Port.CToV.Discover);
+
+                // Отправляем на дрон JSON-запрос и получаем ответ также в виде JSON.
+                stream = connection.GetStream ();
+
+                stream.WriteASCIIString (String.Format (Const.Discovering.CToV_Json,
+                                                        Const.Discovering.ControllerTypeDefault, Const.Discovering.ControllerNameDefault,
+                                                        Const.NetworkConnection.Port.VToC.Port, Const.NetworkConnection.Port.VToC.Stream, Const.NetworkConnection.Port.VToC.Control));
+
+                while (stream.DataAvailable == false) ;
+
+                responseJson = stream.ReadASCIIString (connection);
+
+                // Проверяем значение поля status в пришедшем от дрона JSON.
+                if ((int)JObject.Parse (responseJson).GetValue ("status") == Const.Discovering.VToC_ConnectionIsCorrect)
+                {
+                    successedOperation = true;
+                }
+
+                else successedOperation = false;
+            }
+
+            catch
+            {
+                responseJson = String.Empty;
+                successedOperation = false;
+            }
+
+            finally
+            {
+                if (stream != null)
+                {
+                    stream.Close ();
+                    stream.Dispose ();
+                    connection.Close ();
+                }
+
+                connection.Dispose ();
+            }
+
+            return successedOperation;
         }
 
         public void TakeOff ()
--- Gravicar.ARDrone3/Const.cs
+++ Gravicar.ARDrone3/Const.cs
@@ -1,10 +1,86 @@
-﻿using Gravicar.ARDrone3.Commands;
+﻿using Wormhole.Network;
+
+using Gravicar.ARDrone3.Commands;
 using Gravicar.ARDrone3.Network;
 
 namespace Gravicar.ARDrone3
 {
     internal static class Const
     {
+        /// <summary>
+        /// Параметры сетевых подключений для Parrot ARDrone3.
+        /// </summary>
+        internal static class NetworkConnection
+        {
+            /// <summary>
+            /// Сетевой адрес дрона.
+            /// </summary>
+            internal const string DroneHostAddress = "192.168.42.1";
+
+            /// <summary>
+            /// Порты подключений.
+            /// </summary>
+            internal static class Port
+            {
+                /// <summary>
+                /// От управляющего клиента к устройству (дрону).
+                /// </summary>
+                internal static class CToV
+                {
+                    /// <summary>
+                    /// Порт для поиска дрона и установки с ним первичного соединения.
+                    /// </summary>
+                    internal const int Discover = 44444;
+
+                    /// <summary>
+                    /// Порт для доставки команд от контроллера на дрон.
+                    /// </summary>
+                    internal const int SendCommands = 54321;
+                }
+
+                /// <summary>
+                /// От устройства (дрона) к управляющему клиенту.
+                /// </summary>
+                internal static class VToC
+                {
+                    internal const int Port = 43210;
+
+                    internal const int Stream = 55004;
+
+                    internal const int Control = 55004;
+                }
+            }
+
+            internal static class ConnectionType
+            {
+                internal static class CToV
+                {
+                    internal const EConnectionType SendCommands = EConnectionType.Udp;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Поиск дрона и установка с ним первичного соединения.
+        /// </summary>
+        internal static class Discovering
+        {
+            /// <summary>
+            /// JSON-запрос, отправляемый на дрон при поиске и установке первичного соединения.
+            /// </summary>
+            internal const string CToV_Json = "{{\"controller_type\":\"{0}\", \"controller_name\":\"{1}\", \"d2c_port\":\"{2}\", \"arstream2_client_stream_port\":\"{3}\",\"arstream2_client_control_port\":\"{4}\"}}";
+
+            internal const string ControllerTypeDefault = "controller_type";
+
+            internal const string ControllerNameDefault = "controller_name";
+
+            /// <summary>
+            /// Значение поля status в JSON-запросе, получаемом от дрона после поиска и установки первичного соединения, которое 
+            /// соответствует успешному завершению операции.
+            /// </summary>
+            internal const int VToC_ConnectionIsCorrect = 0;
+        }
+
         internal static class Frame
         {
             internal const int HeaderLength = sizeof (EFrameDataType) + sizeof (EFrameTargetBufferId) + sizeof (byte) + sizeof (int);
